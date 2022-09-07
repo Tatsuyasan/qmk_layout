23ff13f32f2eb0b01671d9a7e1ce47181387dc1a
vincentvermeersch <sk8_vince@hotmail.fr>
2022-09-07 16:19:17 +0200

    Add toggle / layer 8 : qwerty without homerow for gaming

--- README.md
+++ README.md
@@ -3,4 +3,12 @@
 It's my custom layout about lily58.
 This layout is inspired by miryoku layout and add a layer for french accent.
 
+To compile : qmk compile -kb <kb_name>/<version> -km <keymap>
+To flash : qmk flash -kb <your_keyboard>/<version> -km <keymap>
+
+In my env
+
+To compile : qmk compile -kb lily58 -km Tatsuyasan
+To flash : qmk flash -kb lily58 -km Tatsuyasan
+
 Currently ===> Work In Progress
--- keymap.c
+++ keymap.c
@@ -15,6 +15,7 @@
 #define _SYM 5
 #define _FN 6
 #define _FR 7
+#define _QWERTY_NO_HOMEROW 8
 
 void eeconfig_init_user(void) {
   set_unicode_input_mode(UC_LNX);
@@ -120,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
     * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
     * `-----------------------------------------/       /     \      \-----------------------------------------'
-    *                   |      | LT 3 | LT 2 | / LT 1  /       \ LT 5 \  | LT 4 | LT 6 | LT 7 |
+    *                   | LT 8 | LT 3 | LT 2 | / LT 1  /       \ LT 5 \  | LT 4 | LT 6 | LT 7 |
     *                   |      | ESC  | Tab  |/ Space /         \Enter \ |BackSP| DEL  |      |
     *                   `-------------------''-------'           '------''--------------------'
     */
@@ -129,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,           KC_W,          KC_E,          KC_R,           KC_T,                                    KC_Y,         KC_U,           KC_I,           KC_O,            KC_P,              KC_MINS, 
         KC_LSFT,   LGUI_T(KC_A),   LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),   KC_G,                                    KC_H,         LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),    LGUI_T(KC_SCLN),   KC_QUOT, 
         KC_LCTL,   KC_Z,           RALT_T(KC_X),  KC_C,          KC_V,           KC_B,    KC_LBRC,         KC_RBRC,       KC_N,         KC_M,           KC_COMM,        RALT_T(KC_DOT),  KC_SLSH,           KC_RSFT, 
-                                   KC_NO,         LT(3,KC_ESC),  LT(2,KC_TAB),   LT(1,KC_SPC),                            LT(5,KC_ENT), LT(4,KC_BSPC),  LT(6,KC_DEL),   LT(7,KC_NO)),
+                                   TG(8),         LT(3,KC_ESC),  LT(2,KC_TAB),   LT(1,KC_SPC),                            LT(5,KC_ENT), LT(4,KC_BSPC),  LT(6,KC_DEL),   LT(7,KC_NO)),
 
 	[_NAV_KEY] = LAYOUT(RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_AGIN, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_NO, KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_CAPS, KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT, KC_BSPC, KC_DEL, KC_NO),
 
@@ -154,5 +155,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO,       XP(10,11),   KC_NO,       KC_NO,       LSFT_T(KC_NO),   KC_NO,                                  KC_NO,       LSFT_T(KC_NO),   KC_NO,   UC_M_LN,   KC_NO,   KC_NO, 
 		KC_NO,       KC_NO,       KC_NO,       XP(12,13),   KC_NO,           KC_NO,     KC_NO,          KC_NO,       KC_NO,       KC_NO,           KC_NO,   KC_NO,     KC_NO,   KC_NO,
 		                          KC_NO,       KC_NO,       KC_NO,           KC_NO,                                  KC_NO,       KC_NO,           KC_NO,   KC_NO
-	)
+	),
+  [_QWERTY_NO_HOMEROW] = LAYOUT(
+        KC_ESC,    KC_1,           KC_2,          KC_3,          KC_4,           KC_5,                                    KC_6,         KC_7,           KC_8,           KC_9,            KC_0,              KC_GRV, 
+        KC_TAB,    KC_Q,           KC_W,          KC_E,          KC_R,           KC_T,                                    KC_Y,         KC_U,           KC_I,           KC_O,            KC_P,              KC_MINS, 
+        KC_LSFT,   KC_A,           KC_S,          KC_D,          KC_F,           KC_G,                                    KC_H,         KC_J,           KC_K,           KC_L,            KC_SCLN,           KC_QUOT, 
+        KC_LCTL,   KC_Z,           KC_X,          KC_C,          KC_V,           KC_B,    KC_LBRC,         KC_RBRC,       KC_N,         KC_M,           KC_COMM,        KC_DOT,          KC_SLSH,           KC_RSFT, 
+                                   TG(8),         KC_ESC,        KC_TAB,         KC_SPC,                                  KC_ENT,       KC_BSPC,        KC_DEL,         KC_NO),
+
 };

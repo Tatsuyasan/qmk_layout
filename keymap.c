6e47fbba6534674364916b97157f7260e51a9f6d
vincentvermeersch <sk8_vince@hotmail.fr>
2022-09-08 11:25:18 +0200

    fix wrong tab key to Alt key

--- keymap.c
+++ keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * ,-----------------------------------------.                    ,-----------------------------------------.
     * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  ~   |
     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+    * | Alt  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
     * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
     * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 	[_QWERTY] = LAYOUT(
         KC_ESC,    KC_1,           KC_2,          KC_3,          KC_4,           KC_5,                                    KC_6,         KC_7,           KC_8,           KC_9,            KC_0,              KC_GRV, 
-        KC_TAB,    KC_Q,           KC_W,          KC_E,          KC_R,           KC_T,                                    KC_Y,         KC_U,           KC_I,           KC_O,            KC_P,              KC_MINS, 
+        KC_LALT,   KC_Q,           KC_W,          KC_E,          KC_R,           KC_T,                                    KC_Y,         KC_U,           KC_I,           KC_O,            KC_P,              KC_MINS, 
         KC_LSFT,   LGUI_T(KC_A),   LALT_T(KC_S),  LCTL_T(KC_D),  LSFT_T(KC_F),   KC_G,                                    KC_H,         LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),    LGUI_T(KC_SCLN),   KC_QUOT, 
         KC_LCTL,   KC_Z,           RALT_T(KC_X),  KC_C,          KC_V,           KC_B,    KC_LBRC,         KC_RBRC,       KC_N,         KC_M,           KC_COMM,        RALT_T(KC_DOT),  KC_SLSH,           KC_RSFT, 
                                    TG(8),         LT(3,KC_ESC),  LT(2,KC_TAB),   LT(1,KC_SPC),                            LT(5,KC_ENT), LT(4,KC_BSPC),  LT(6,KC_DEL),   LT(7,KC_NO)),
@@ -158,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
   [_QWERTY_NO_HOMEROW] = LAYOUT(
         KC_ESC,    KC_1,           KC_2,          KC_3,          KC_4,           KC_5,                                    KC_6,         KC_7,           KC_8,           KC_9,            KC_0,              KC_GRV, 
-        KC_TAB,    KC_Q,           KC_W,          KC_E,          KC_R,           KC_T,                                    KC_Y,         KC_U,           KC_I,           KC_O,            KC_P,              KC_MINS, 
+        KC_LALT,   KC_Q,           KC_W,          KC_E,          KC_R,           KC_T,                                    KC_Y,         KC_U,           KC_I,           KC_O,            KC_P,              KC_MINS, 
         KC_LSFT,   KC_A,           KC_S,          KC_D,          KC_F,           KC_G,                                    KC_H,         KC_J,           KC_K,           KC_L,            KC_SCLN,           KC_QUOT, 
         KC_LCTL,   KC_Z,           KC_X,          KC_C,          KC_V,           KC_B,    KC_LBRC,         KC_RBRC,       KC_N,         KC_M,           KC_COMM,        KC_DOT,          KC_SLSH,           KC_RSFT, 
                                    TG(8),         KC_ESC,        KC_TAB,         KC_SPC,                                  KC_ENT,       KC_BSPC,        KC_DEL,         KC_NO),

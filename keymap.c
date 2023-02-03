5cc69fbbb1a1850f12ef1c54acd661ab14e3009a
Vincent <sk8_vince@hotmail.fr>
2023-02-03 12:35:53 +0100

    update keymap

--- keymap.c
+++ keymap.c
@@ -165,11 +165,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,      KC_NO,          KC_F1,         KC_F2,         KC_F3,           KC_NO,          KC_NO,        KC_NO,           KC_NO,           KC_NO,            KC_NO,          KC_RALT,            KC_NO,            KC_NO, 
                                 KC_NO,         KC_APP,        KC_TAB,          KC_SPC,                                        KC_NO,           KC_NO,            KC_NO,          KC_NO),
     [_FR] = LAYOUT(
-	KC_NO,      KC_NO,          KC_NO,         KC_NO,         KC_NO,           KC_NO,                                         KC_NO,           KC_NO,            KC_NO,          KC_NO,              KC_NO,            KC_NO, 
-	KC_NO,      KC_NO,          UC_M_WC,       XP(0,1),       KC_NO,           KC_NO,                                         KC_NO,           KC_NO,            KC_NO,          KC_NO,              KC_NO,            KC_NO, 
-	KC_NO,      XP(10,11),      KC_NO,         KC_NO,         LSFT_T(KC_NO),   KC_NO,                                         KC_NO,           LSFT_T(KC_NO),    KC_NO,          UC_M_LN,            KC_NO,            KC_NO, 
-	KC_NO,      KC_NO,          KC_NO,         XP(12,13),     KC_NO,           KC_NO,          KC_NO,        KC_NO,           KC_NO,           KC_NO,            KC_NO,          KC_NO,              KC_NO,            KC_NO,
-		                        KC_NO,         KC_NO,         KC_NO,           KC_SPC,                                        KC_NO,           KC_NO,            KC_NO,          KC_NO),
+    KC_NO,      KC_NO,          KC_NO,         KC_NO,         KC_NO,           KC_NO,                                         KC_NO,           KC_NO,            KC_NO,          KC_NO,              KC_NO,            KC_NO, 
+    KC_NO,      KC_NO,          UC_M_WC,       XP(0,1),       KC_NO,           KC_NO,                                         KC_NO,           KC_NO,            KC_NO,          KC_NO,              KC_NO,            KC_NO, 
+    KC_NO,      XP(10,11),      KC_NO,         KC_NO,         LSFT_T(KC_NO),   KC_NO,                                         KC_NO,           LSFT_T(KC_NO),    KC_NO,          UC_M_LN,            KC_NO,            KC_NO, 
+    KC_NO,      KC_NO,          KC_NO,         XP(12,13),     KC_NO,           KC_NO,          KC_NO,        KC_NO,           KC_NO,           KC_NO,            KC_NO,          KC_NO,              KC_NO,            KC_NO,
+    	                        KC_NO,         KC_NO,         KC_NO,           KC_SPC,                                        KC_NO,           KC_NO,            KC_NO,          KC_NO),
     [_QWERTY_NO_HOMEROW] = LAYOUT(
     KC_ESC,     KC_1,           KC_2,          KC_3,          KC_4,            KC_5,                                          KC_6,            KC_7,             KC_8,           KC_9,               KC_0,             KC_GRV, 
     KC_LALT,    KC_Q,           KC_W,          KC_E,          KC_R,            KC_T,                                          KC_Y,            KC_U,             KC_I,           KC_O,               KC_P,             KC_MINS, 
